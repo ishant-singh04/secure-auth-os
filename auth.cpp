3bceb0577c1091c40f6fc4cc0443edde379e91f7
ishant-singh04 <ishantsingh1955@gmail.com>
2025-12-15 21:04:03 +0530

    Added basic authentication logic

--- auth.cpp
+++ auth.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
+#include <map>
+
 using namespace std;
 
+class SecureAuth {
+private:
+    map<string, string> users;
+
+public:
+    SecureAuth() {
+        users["admin"] = "Admin@123";
+        users["user"]  = "User@123";
+    }
+
+    bool authenticate() {
+        string username, password;
+        cout << "Enter Username: ";
+        cin >> username;
+        cout << "Enter Password: ";
+        cin >> password;
+
+        return users[username] == password;
+    }
+};
+
 int main() {
-    cout << "Secure Authentication Module for OS\n";
-    return 0;
+    SecureAuth auth;
+    if (auth.authenticate())
+        cout << "Login successful\n";
+    else
+        cout << "Login failed\n";
 }
